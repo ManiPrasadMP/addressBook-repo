31ac2977e4d8510a629a73745fa1a1097ccd5da2
ManiPrasadMP <maniprasadkr08@gmail.com>
2025-08-18 14:52:00 +0530

    contact-creation

--- AddressBook.java
+++ AddressBook.java
@@ -2,6 +2,20 @@ package adress_book;
 
 public class AddressBook {
     public static void main(String[] args) {
-        System.out.println("Welcome to Address Book Program!");
+    	System.out.println("Welcome to Address Book Program!");
+
+        
+        Contact contact = new Contact(
+                "John",
+                "Doe",
+                "123 Street",
+                "New York",
+                "NY",
+                "10001",
+                "1234567890",
+                "john.doe@example.com"
+        );
+
+        System.out.println(contact);
     }
 }

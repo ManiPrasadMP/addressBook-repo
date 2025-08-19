f048f9de163c743ba7228f3dc3a73244c1f1d0e9
ManiPrasadMP <maniprasadkr08@gmail.com>
2025-08-19 12:00:47 +0530

    additional-changes

--- AddressBook.java
+++ AddressBook.java
@@ -2,21 +2,46 @@ package adress_book;
 
 import java.util.ArrayList;
 import java.util.Iterator;
-import java.util.List;
 import java.util.Scanner;
 
 public class AddressBook {
-    private List<Contact> contacts;
+	 private String name;
+    private ArrayList<Contact> contacts;
 
-    public AddressBook() {
-        contacts = new ArrayList<>();
+    public AddressBook(String name) {
+        this.name = name;
+        this.contacts = new ArrayList<>();
     }
 
-    public void addContact(Contact contact) {
+    public String getName() { return name; }
+
+    public void addContact(Scanner sc) {
+    	System.out.print("Enter First Name: ");
+        String firstName = sc.nextLine();
+        System.out.print("Enter Last Name: ");
+        String lastName = sc.nextLine();
+        System.out.print("Enter Address: ");
+        String address = sc.nextLine();
+        System.out.print("Enter City: ");
+        String city = sc.nextLine();
+        System.out.print("Enter State: ");
+        String state = sc.nextLine();
+        System.out.print("Enter Zip: ");
+        String zip = sc.nextLine();
+        System.out.print("Enter Phone Number: ");
+        String phoneNumber = sc.nextLine();
+        System.out.print("Enter Email: ");
+        String email = sc.nextLine();
+
+        Contact contact= new Contact(firstName, lastName, address, city, state, zip, phoneNumber, email);
         contacts.add(contact);
     }
 
-    public void editContact(String firstName, String lastName, Scanner sc) {
+    public void editContact( Scanner sc) {
+    	System.out.print("Enter First Name of contact to edit: ");
+        String firstName = sc.nextLine();
+        System.out.print("Enter Last Name of contact to edit: ");
+        String lastName = sc.nextLine();
         for (Contact contact : contacts) {
             if (contact.getFirstName().equalsIgnoreCase(firstName) && contact.getLastName().equalsIgnoreCase(lastName)) {
                 System.out.print("Enter new Address: ");
@@ -38,7 +63,11 @@ public class AddressBook {
         System.out.println("Contact not found!");
     }
     
-    public void deleteContact(String firstName, String lastName) {
+    public void deleteContact(Scanner sc) {
+    	System.out.print("Enter First Name of contact to delete: ");
+        String firstName = sc.nextLine();
+        System.out.print("Enter Last Name of contact to delete: ");
+        String lastName = sc.nextLine();
         Iterator<Contact> iterator = contacts.iterator();
         while (iterator.hasNext()) {
             Contact contact = iterator.next();
